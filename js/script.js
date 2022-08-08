2ac280f6ffa7ad173d1303f453d58f7b49eed450
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-08-09 00:20:02 +0300

    added last tasks

--- js/script.js
+++ js/script.js
@@ -1,98 +1,163 @@
 'use strict';
 
 
-////////////    Task #1
+// ////////////    Task #1
 
-const  searchString = (originalStr, str) => {
+// const  searchString = (originalStr, str) => {
 
-    if (originalStr.includes(str)) {
-        return true;
-    } else {
-        return false;
-    }
+//     if (originalStr.includes(str)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
     
-}
+// }
 
-console.log(searchString('hello', 'www'));
+// console.log(searchString('hello', 'www'));
+
+
+// /////////////
+
+// ////////////        Task #2
+
+
+// function capitalizeFirstLetter (str) {
+//     let upperLetter = str[0].toUpperCase() + str.slice(1);
+
+//     return upperLetter;
+// }
+
+
+// // function capitalizeFirstLetter (str) {
+// //     let letter = str[0];
+// //     console.log(letter);
+
+// //     let upperLetter = letter.toUpperCase();
+// //     console.log(upperLetter);
+
+// //     // letter = [upperLetter];
+
+// //     str
+
+// //     console.log(letter);
+
+// //     return;
+    
+// // }
+
+// console.log(capitalizeFirstLetter('hello'));
+
+// // console.log('hello'[0].toUpperCase());
+
+
+// ///////////////////
+
+// /////////////////       Task #3 решение подсмотрел, до концна не разобрался.
+
+
+// let trimString = function (string, length) {
+//     return string.length > length ? 
+//            string.substring(0, length) + '...' :
+//            string;
+//   };
+
+// console.log(trimString('lorem hello', 5));
+
+
+// ////////////////
+
+// ///////////////     Task #4
+
+
+// const students = [
+//     {
+//         name: 'John Smith',
+//         marks: [10, 8, 6, 9, 8, 7 ]
+//     },
+//     {
+//         name: 'John Doe',
+//         marks: [ 9, 8, 7, 6, 7 ]
+//     },
+//     {
+//         name: 'Thomas Anderson',
+//         marks: [6, 7, 10, 8 ]
+//     },
+//     {
+//         name: 'Jean-Baptiste Emanuel Zorg',
+//         marks: [10, 9, 8, 9 ]
+//     }
+// ]
+
+// students.forEach(student => {
+
+//     let averageMark = student.marks.reduce((acc, marks) => acc + marks) / student.marks.length;
+    
+//     return student.averageMark = averageMark;
+// });
+
+// console.log(students);
 
 
 /////////////
 
-////////////        Task #2
+////////////        Task #5     чет запутался, и не могу понять че делать дальше.
 
+// const vehicles = [
+//     { make: 'Honda', model: 'CR-V', type: 'suv', price: 24045 },
+//     { make: 'Honda', model: 'Accord', type: 'sedan', price: 22455 },
+//     { make: 'Mazda', model: 'Mazda 6', type: 'sedan', price: 24195 },
+//     { make: 'Mazda', model: 'CX-9', type: 'suv', price: 31520 },
+//     { make: 'Toyota', model: '4Runner', type: 'suv', price: 34210 },
+//     { make: 'Toyota', model: 'Sequoia', type: 'SUV', price: 45560 },
+//     { make: 'Toyota', model: 'Tacoma', type: 'truck', price: 24320 },
+//     { make: 'Ford', model: 'F-150', type: 'truck', price: 27110 },
+//     { make: 'Ford', model: 'Fusion', type: 'sedan', price: 22120 },
+//     { make: 'Ford', model: 'Explorer', type: 'SUV', price: 31660 }
+//   ];
 
-function capitalizeFirstLetter (str) {
-    let upperLetter = str[0].toUpperCase() + str.slice(1);
+//   const filterVehicles = vehicles.filter((vehicle) => {
 
-    return upperLetter;
-}
+//     let filtLowerCase = vehicle.type = vehicle.type.toLowerCase();
 
-
-// function capitalizeFirstLetter (str) {
-//     let letter = str[0];
-//     console.log(letter);
-
-//     let upperLetter = letter.toUpperCase();
-//     console.log(upperLetter);
-
-//     // letter = [upperLetter];
-
-//     str
-
-//     console.log(letter);
-
-//     return;
+//     if (filtLowerCase === 'suv') {
+//         return filtLowerCase;
+//     }
+//   });
     
+//   console.log(filterVehicles);
+
+//   filterVehicles.forEach(car => {
+//     let s = car.price
+//   });
+
+// console.log(filterVehicles());
+
+
+/////////
+
+//////////////      Task #6
+
+// const user = {
+//     name: 'John', age: 22, salary: null,
 // }
 
-console.log(capitalizeFirstLetter('hello'));
+// let getKeysCount = () => {
+//     return Object.keys(user).length;
+// }
 
-// console.log('hello'[0].toUpperCase());
-
-
-///////////////////
-
-/////////////////       Task #3 решение подсмотрел, до концна не разобрался.
+// console.log(getKeysCount());
 
 
-let trimString = function (string, length) {
-    return string.length > length ? 
-           string.substring(0, length) + '...' :
-           string;
-  };
+/////////////
 
-console.log(trimString('lorem hello', 5));
+////////////        Task #7
 
 
-////////////////
+let arr = [10, 20, 3, 4, 0, 99, 97];
+let sorted = arr.slice();
 
-///////////////     Task #4
+sorted.sort((a, b) => a - b);
 
+console.log(sorted);
 
-const students = [
-    {
-        name: 'John Smith',
-        marks: [10, 8, 6, 9, 8, 7 ]
-    },
-    {
-        name: 'John Doe',
-        marks: [ 9, 8, 7, 6, 7 ]
-    },
-    {
-        name: 'Thomas Anderson',
-        marks: [6, 7, 10, 8 ]
-    },
-    {
-        name: 'Jean-Baptiste Emanuel Zorg',
-        marks: [10, 9, 8, 9 ]
-    }
-]
-
-students.forEach(student => {
-
-    let averageMark = student.marks.reduce((acc, marks) => acc + marks) / student.marks.length;
-    
-    return student.averageMark = averageMark;
-});
-
-console.log(students);
+console.log(arr);
